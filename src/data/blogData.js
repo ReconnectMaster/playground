735ec2f6fbe1b6586e5e50ceecf6b2c7065c5e20
Raccoon <chaleeart@gmail.com>
2026-08-19 04:02:57 +0700

    Remove unused blog post import

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,10 +1,7 @@
 // 1. Plug in each individual light bulb (JSON post)
 import post1 from './posts/gmail-delete-rename.json';
-import post2 from './posts/future-of-audio.json';
 
 // 2. Control which lights are turned ON across the whole site
 export const blogPosts = [
-  post1,
-  post2
+  post1
 ];
-
